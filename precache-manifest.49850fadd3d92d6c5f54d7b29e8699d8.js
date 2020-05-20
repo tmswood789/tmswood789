ec7dd2b4e662f069667a8e1f27bedf1aaae2f7ac
tmswood789 <65664769+tmswood789@users.noreply.github.com>
2020-05-20 16:27:32 +0300

    Add files via upload

---
+++ precache-manifest.49850fadd3d92d6c5f54d7b29e8699d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c57ac796460f910f8582a6145a2f67a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "750d8cb40353c243d239",
+    "url": "/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "750d8cb40353c243d239",
+    "url": "/static/js/2.b45837c7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b45837c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b840b0c3ca9a983325f",
+    "url": "/static/js/main.2c9367e3.chunk.js"
+  },
+  {
+    "revision": "db2fb79c6dd819591e3e",
+    "url": "/static/js/runtime-main.b1496188.js"
+  }
+]);
